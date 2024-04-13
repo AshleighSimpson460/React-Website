3ed3a57eda678ba984962bdd52d520d801e9ae7e
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2024-04-13 13:01:42 +0100

    Added logo to nav + removed the colour of the background.

--- game-hub/src/App.js
+++ game-hub/src/App.js
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show, useBreakpoint } from "@chakra-ui/react";
 import { useBreakpointValue } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   const showAside = useBreakpointValue({ base: false, lg: true });
@@ -10,8 +11,8 @@ function App() {
         lg: `"nav nav" "aside main"`, //larger than 1024px
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <Navbar />
       </GridItem>
       {showAside && (
         <GridItem area="aside" bg="gold">
