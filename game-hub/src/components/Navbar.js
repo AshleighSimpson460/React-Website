12911305621759c711fdc9edbf197b5b2f494262
AshleighSimpson460 <118854789+AshleighSimpson460@users.noreply.github.com>
2024-04-13 13:15:19 +0100

    built colormode switch

--- game-hub/src/components/Navbar.js
+++ game-hub/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import { px } from "framer-motion";
 
 function Navbar() {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
